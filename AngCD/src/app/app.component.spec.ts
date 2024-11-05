65d62cebec812c1ae85ef751e525b69b13fc10e3
MuhammedTANAY <MuhammedTNY@gmail.com>
2024-11-05 17:02:09 +0300

    değiştir herşer

--- AngCD/src/app/app.component.spec.ts
+++ AngCD/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AngCd' title`, () => {
+  it(`should have as title 'proje1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngCd');
+    expect(app.title).toEqual('proje1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngCd');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('proje1 app is running!');
   });
 });
